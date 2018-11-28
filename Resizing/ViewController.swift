1c508fa950bfb14f7c9de751b27587258d73f58b
Srinivas Rowdur <lonsr28@lonnr8-w1.paconsulting.com>
2018-11-28 11:24:46 +0000

    Initial Commit

---
+++ Resizing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resizing
+//
+//  Created by Srinivas Rowdur on 28/11/2018.
+//  Copyright © 2018 Srinivas Rowdur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
